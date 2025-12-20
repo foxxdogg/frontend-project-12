fe00c6ba128621ff8299f258e1450fdb5b5b8227
foxdog <alexkolymago@gmail.com>
2025-12-20 18:40:44 +0300

    step12: fix

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,71 +1,9 @@
-// /* eslint-disable import/no-extraneous-dependencies */
-
-// import { FlatCompat } from '@eslint/eslintrc'
-// import importPlugin from 'eslint-plugin-import'
-// import jsxA11y from 'eslint-plugin-jsx-a11y'
-// import globals from 'globals'
-
-// const compat = new FlatCompat({ baseDir: process.cwd() })
-
-// export default [
-//   // Игнорируем папки
-//   { ignores: ['node_modules/', 'dist/', 'build/'] },
-
-//   {
-//     plugins: {
-//       import: importPlugin,
-//       'jsx-a11y': jsxA11y,
-//     },
-//   },
-
-//   // AirBnB правила
-//   ...compat.extends('airbnb'),
-//   // React правила
-//   ...compat.extends('plugin:react/recommended'),
-//   // React Hooks правила
-//   ...compat.extends('plugin:react-hooks/recommended'),
-
-//   // Функциональные правила подключаем напрямую
-//   {
-//     files: ['**/*.js', '**/*.jsx'],
-//     languageOptions: {
-//       ecmaVersion: 'latest',
-//       sourceType: 'module',
-//       globals: {
-//         ...globals.browser,
-//         ...globals.node,
-//       },
-//     },
-//     rules: {
-//       semi: ['error', 'never'],
-//       'functional/no-conditional-statements': 'off',
-//       'functional/no-expression-statements': 'off',
-//       'functional/immutable-data': 'off',
-//       'functional/functional-parameters': 'off',
-//       'functional/no-try-statements': 'off',
-//       'functional/no-throw-statements': 'off',
-//       'functional/no-return-void': 'off',
-//       'import/prefer-default-export': 'off',
-
-//       'import/extensions': 'off',
-//       'import/no-unresolved': 'off',
-//       'react/prop-types': 'off',
-//       'no-console': 'off',
-//       'react/react-in-jsx-scope': 'off',
-//       'no-underscore-dangle': ['error', { allow: ['__filename', '__dirname'] }],
-//       'react/function-component-definition': ['error', { namedComponents: 'arrow-function' }],
-//       'testing-library/no-debug': 'off',
-//       'react/jsx-filename-extension': ['warn', { extensions: ['.js', '.jsx'] }],
-//     },
-//   },
-// ]
 import js from '@eslint/js'
 import globals from 'globals'
 import react from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
 import jsxA11y from 'eslint-plugin-jsx-a11y'
 import importPlugin from 'eslint-plugin-import'
-import stylistic from '@stylistic/eslint-plugin'
-import reactHooks from 'eslint-plugin-react-hooks'
 
 export default [
   {
@@ -81,16 +19,10 @@ export default [
       'react-hooks': reactHooks,
       'jsx-a11y': jsxA11y,
       import: importPlugin,
-      '@stylistic': stylistic,
     },
     languageOptions: {
       ecmaVersion: 'latest',
       sourceType: 'module',
-      parserOptions: {
-        ecmaFeatures: {
-          jsx: true,
-        },
-      },
       globals: {
         ...globals.browser,
         ...globals.node,
@@ -102,26 +34,20 @@ export default [
       },
     },
     rules: {
-      // стиль
-      '@stylistic/semi': ['error', 'never'],
-      '@stylistic/arrow-parens': ['error', 'as-needed'],
-      '@stylistic/brace-style': ['error', '1tbs'],
-      '@stylistic/indent': ['error', 2],
-      '@stylistic/multiline-ternary': ['error', 'always-multiline'],
-      '@stylistic/quote-props': ['error', 'as-needed'],
-      'react/jsx-uses-vars': 'error',
-      'react/jsx-uses-react': 'off',
-
-      // react
+      // React
       'react/react-in-jsx-scope': 'off',
       'react/prop-types': 'off',
 
-      // import
+      // Hooks
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+
+      // Import
       'import/extensions': 'off',
       'import/no-unresolved': 'off',
       'import/no-extraneous-dependencies': 'off',
 
-      // прочее
+      // General
       'no-console': 'off',
     },
   },
