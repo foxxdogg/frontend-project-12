8c51b429f39e3577cc26a87c97dc7d6c15962057
foxdog <alexkolymago@gmail.com>
2025-12-22 00:01:59 +0300

    step12: fix

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -6,18 +6,16 @@ import globals from 'globals'
 const compat = new FlatCompat({ baseDir: new URL('.', import.meta.url).pathname })
 
 export default [
-  // Игнорируем папки
+  
   {
     ignores: ['node_modules/', 'dist/', 'build/'],
   },
 
-  // AirBnB + React + React Hooks через FlatCompat
   ...compat.extends('airbnb', 'plugin:react/recommended', 'plugin:react-hooks/recommended'),
 
-  // Настройка плагинов и глобальных переменных
   {
     plugins: {
-      import: importPlugin,
+      'import': importPlugin,
       'jsx-a11y': jsxA11y,
     },
     languageOptions: {
@@ -35,7 +33,6 @@ export default [
     },
   },
 
-  // Специфические файлы
   {
     files: ['eslint.config.js', 'vite.config.js'],
     rules: {
@@ -43,17 +40,16 @@ export default [
     },
   },
 
-  // Правила проекта
   {
     rules: {
-      semi: ['error', 'never'], // точки с запятой отключены
+      'semi': ['error', 'never'],
       'react/prop-types': 'off',
       'react/react-in-jsx-scope': 'off',
       'import/prefer-default-export': 'off',
       'import/extensions': 'off',
       'import/no-unresolved': 'off',
       'no-console': 'off',
-      'quote-props': ['error', 'as-needed'], // кавычки только если нужно
+      'quote-props': ['error', 'as-needed'],
     },
   },
 ]
